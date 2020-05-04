0ff5dbb0d748a58ba2916ecac488e2f799617d19
1209simran <1209simran@gmail.com>
2020-05-04 20:06:13 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -5,22 +5,11 @@ import Resume from "./components/Resume";
 import Portfolio from "./components/Portfolio";
 import Footer from "./components/Footer";
 import resumeData from "./resumeData";
-import file2 from "../src/bensound-perception.mp3";
-import Sound from "react-sound";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Sound
-          url={file2}
-          autoLoad={true}
-          playStatus={Sound.status.PLAYING}
-          playFromPosition={300}
-          onLoading={this.handleSongLoading}
-          onPlaying={this.handleSongPlaying}
-          onFinishedPlaying={this.handleSongFinishedPlaying}
-        />
         <Header resumeData={resumeData} />
 
         <About resumeData={resumeData} />

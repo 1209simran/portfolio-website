474a72d84a1767e717f127feadea1b7e1ab9a829
1209simran <1209simran@gmail.com>
2020-05-04 20:16:20 +0530

    Updates

---
+++ precache-manifest.d573f848aee1cc82f19f723a3dcc596c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b6b17faf27c71f0d4ca",
+    "url": "/Portfolio-Website/static/js/runtime~main.9b6b17fa.js"
+  },
+  {
+    "revision": "8168c95fcd5acea54ebf",
+    "url": "/Portfolio-Website/static/js/main.8168c95f.chunk.js"
+  },
+  {
+    "revision": "1b9bdbcd6793fc65d3dd",
+    "url": "/Portfolio-Website/static/js/1.1b9bdbcd.chunk.js"
+  },
+  {
+    "revision": "8168c95fcd5acea54ebf",
+    "url": "/Portfolio-Website/static/css/main.5c9df105.chunk.css"
+  },
+  {
+    "revision": "08c0f738bcaafd08445a79bc2c7f9880",
+    "url": "/Portfolio-Website/index.html"
+  }
+];

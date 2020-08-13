9b4dc190574ee5d8df6e4233ed73897db59f80f2
1209simran <1209simran@gmail.com>
2020-08-13 07:17:14 +0530

    bug fixed

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -25,7 +25,7 @@ export default class Resume extends Component {
                           {item.MonthOfPassing} {item.YearOfPassing}
                         </em>
                       </p>
-                      <p>{item.Achievements}</p>
+                      <p className="info1">{item.Achievements}</p>
                     </div>
                   </div>
                 );
